cfc6660f620990e896f292b168c39bb976c8e746
eyefkey <cutamoraharley@gmail.com>
2023-12-10 22:02:40 +0800

    html added

--- faculty/dashboard/add_activity.php
+++ faculty/dashboard/add_activity.php
@@ -57,3 +57,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Close the database connection
 $conn->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Task</title>
+    <!-- Include your CSS stylesheets or link to external stylesheets here -->
+    <link rel="stylesheet" href="/sol/faculty/css/faculty.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+</head>
+<body>
+    <main class="form">
+        <section class="form__header">
+            <h1>Add Task</h1>
+        </section>
+        <section class="form__body">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <input type="hidden" name="rec_ID" value="<?php echo htmlspecialchars($_GET['rec_ID']); ?>">
+                
+                <label for="sub_ID">Subject Code:</label>
+                <input type="text" name="sub_ID" required>
+
+                <label for="task_name">Task Name:</label>
+                <input type="text" name="task_name" required>
+
+                <label for="task_date">Task Date:</label>
+                <input type="date" name="task_date" required>
+
+                <label for="task_description">Task Description:</label>
+                <textarea name="task_description" rows="4" required></textarea>
+
+                <button type="submit">Add Task</button>
+            </form>
+        </section>
+    </main>
+    <!-- Include your navigation bar or other elements as needed -->
+    <nav>
+        <ul>
+            <li>
+                <a href="#" class="logo">
+                    <img src="/sol/img/sol.png" alt="">
+                    <span class="nav-item"> SOL - CRMS</span>
+                </a>
+            </li>
+            <!-- Include other navigation links as needed -->
+        </ul>
+    </nav>
+    <!-- Include your JavaScript files or scripts as needed -->
+</body>
+</html>
