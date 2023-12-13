a10950907ee0183d76e01533980be65895995b9d
eyefkey <cutamoraharley@gmail.com>
2023-12-14 04:07:38 +0800

    analytical add

--- faculty/analytic/analytic.php
+++ faculty/analytic/analytic.php
@@ -10,22 +10,36 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$query = $conn->query("SELECT r.sub_ID, COUNT(CASE WHEN g.final_grade >= 75 THEN g.rec_ID END) as passed_count, COUNT(CASE WHEN g.final_grade < 75 THEN g.rec_ID END) as failed_count FROM rec_info r JOIN grade_info g ON r.rec_ID = g.rec_ID GROUP BY r.sub_ID");
+// Query to get the number of students passed and failed for each sub_ID
+$query = $conn->query("SELECT r.sub_ID, 
+    COUNT(CASE WHEN g.final_grade >= 75 THEN g.rec_ID END) as passed_count, 
+    COUNT(CASE WHEN g.final_grade < 75 THEN g.rec_ID END) as failed_count 
+    FROM rec_info r JOIN grade_info g ON r.rec_ID = g.rec_ID 
+    GROUP BY r.sub_ID");
+
 $data = $query->fetch_all(MYSQLI_ASSOC);
 
 $subIDs = array_column($data, 'sub_ID');
 $passedCounts = array_column($data, 'passed_count');
-$failedCounts = array_column($data, 'failed_count');
+
+// Query to get the number of enrolled students for each sub_ID
+$enrollmentQuery = $conn->query("SELECT sub_ID, COUNT(DISTINCT stud_ID) AS enrolled_students_count
+    FROM rec_info
+    GROUP BY sub_ID");
+
+$enrollmentData = $enrollmentQuery->fetch_all(MYSQLI_ASSOC);
+$enrolledStudentsCounts = array_column($enrollmentData, 'enrolled_students_count');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="/sol/faculty/css/analytics.css"/>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/sol/faculty/css/analytic.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
 <nav>
@@ -50,47 +64,77 @@ $failedCounts = array_column($data, 'failed_count');
     </ul>
 </nav>
 
-</body>
-<div class="chart-container" style="width: 500px; height: 300px;">
-  <canvas id="myChart"></canvas>
+<div class="room">
+<div class="chart-container1" style="width: 400px; height: 400px;">
+    <canvas id="polarChart"></canvas>
+</div>
+
+<div class="chart-container2" style="width: 400px; height: 200px;">
+    <canvas id="lineChart"></canvas>
+</div>
 </div>
 <script>
-const data = {
-  labels: <?php echo json_encode($subIDs); ?>,
-  datasets: [{
-    label: 'Students Passed',
-    data: <?php echo json_encode($passedCounts); ?>,
-    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-    borderColor: 'rgb(75, 192, 192)',
-    borderWidth: 1
-  },
-  {
-    label: 'Students Failed',
-    data: <?php echo json_encode($failedCounts); ?>,
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgb(255, 99, 132)',
-    borderWidth: 1
-  }]
+const polarData = {
+    labels: <?php echo json_encode($subIDs); ?>,
+    datasets: [{
+        label: 'Students Passed',
+        data: <?php echo json_encode($passedCounts); ?>,
+        backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)'
+      ],
+        borderWidth: 1
+    }]
 };
 
-const config = {
-  type: 'bar',
-  data: data,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-        ticks: {
-          stepSize: 1
+const polarConfig = {
+    type: 'polarArea',
+    data: polarData,
+    options: {
+        scales: {
+            r: {
+                suggestedMin: 0
+            }
         }
-      }
     }
-  }
 };
 
-var myChart = new Chart(
-  document.getElementById('myChart'),
-  config
+const lineData = {
+    labels: <?php echo json_encode($subIDs); ?>,
+    datasets: [{
+        label: 'Enrolled Students',
+        data: <?php echo json_encode($enrolledStudentsCounts); ?>,
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+};
+
+const lineConfig = {
+    type: 'line',
+    data: lineData,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                stepSize: 1
+            }
+        }
+    }
+};
+
+var polarChart = new Chart(
+    document.getElementById('polarChart'),
+    polarConfig
+);
+
+var lineChart = new Chart(
+    document.getElementById('lineChart'),
+    lineConfig
 );
 </script>
+</body>
 </html>
