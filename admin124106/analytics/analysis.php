42e7cb4a11ed7858e56babfe4b0153e8f69e07df
eyefkey <cutamoraharley@gmail.com>
2023-12-31 10:06:56 +0800

    fix

--- admin124106/analytics/analysis.php
+++ admin124106/analytics/analysis.php
@@ -114,7 +114,13 @@ const barData = {
     datasets: [{
         label: 'Enrolled Students',
         data: <?php echo json_encode($enrolledStudentsCounts); ?>,
-        backgroundColor: 'rgb(75, 192, 192)',
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(75, 192, 192)',
+            'rgb(255, 205, 86)',
+            'rgb(201, 203, 207)',
+            'rgb(54, 162, 235)'
+        ],
         borderWidth: 1
     }]
 };
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="index.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="indexm.css">
     <title>Login</title>
 </head>
 <body>
